7ba50a1ec4a9171ecf49f35f7ee815456846a4ce
Milan Vojnovic <milanvojnovic@gmail.com>
2016-11-12 14:13:42 +0100

    runnable via python3 app.py

--- app.py
+++ app.py
@@ -18,19 +18,24 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.add_url_rule()
-
 
 @app.route('/', methods=['GET'])
 def list_tickets():
     """Display a list of tickets in the system."""
+    return 'list_tickets'
 
 
 @app.route('/ticket', methods=['GET', 'POST'])
 def enter_ticket():
     """Enter a new ticket via a form."""
+    return 'enter_ticket'
 
 
 @app.route('/ticket/<ticket_id>')
 def display_ticket(ticket_id):
     """Display the details of the ticket with id *ticket_id*."""
+    return 'display_ticket({ticket_id})'.format(ticket_id=ticket_id)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
