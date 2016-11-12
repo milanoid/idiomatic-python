f7294316ed098cc7110a1e1d4b1c0f6a49566b03
Milan Vojnovic <milanvojnovic@gmail.com>
2016-11-12 17:00:35 +0100

    submitting the form creates a new record in db and redirects to list of all tickets

--- app.py
+++ app.py
@@ -15,10 +15,10 @@ COMMIT;
 """
 
 import sqlite3
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+INSERT_STATEMENT = """INSERT INTO Ticket VALUES ({id}, "{change}", "{title}", "{description}", "{submitter_name}", "{submitter_email}", "{submitter_website}","{file}", "{date_created}")"""
 
 
 @app.route('/', methods=['GET'])
@@ -35,6 +35,22 @@ def list_tickets():
 def enter_ticket():
     if request.method == 'GET':
         return render_template('edit.html')
+    else:
+        connection = sqlite3.connect('db.sqlite3')
+        cursor = connection.cursor()
+        cursor.execute(INSERT_STATEMENT.format(
+            id=request.form.get('id'),
+            change=request.form.get('change'),
+            title=request.form.get('title'),
+            description=request.form.get('description'),
+            submitter_name=request.form.get('submitter_name'),
+            submitter_email=request.form.get('submitter_email'),
+            submitter_website=request.form.get('submitter_website'),
+            file=request.form.get('file'),
+            date_created=request.form.get('date_created')
+        ))
+        connection.commit()
+        return redirect(url_for('list_tickets'))
 
 
 @app.route('/ticket/<int:ticket_id>')
